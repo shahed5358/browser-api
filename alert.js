b60feca4a1b1161fec0bb34f15ebca27be5a827e
shahed5358e <mohammadshahed5358@gmail.com>
2022-03-06 20:17:20 +0600

    alert location js add

---
+++ alert.js
@@ -0,0 +1,23 @@
+console.log(' i am here');
+alert('Ma is coming!!');
+const maComing = () => {
+    alert('ammu is coming. Open the book');
+}
+
+const askPicnic = () => {
+    const response = confirm('are you going to picnic?');
+    console.log(response);
+    if (response === true) {
+        alert('amk fee ta bkash kor ');
+    }
+    else {
+        console.log('dore giye mor')
+    }
+}
+
+const askName = () => {
+    const name = prompt('what is your name?');
+    if (name === true) {
+        console.log(name);
+    }
+}
---
+++ explore.js
@@ -0,0 +1,10 @@
+
+const name = 15;
+const student = {
+    name: 'shahed', friend: 'foysal', job: 'business', makeMovie: function (name) {
+        console.log(name);
+    }
+}
+student.makeMovie('din rat');
+document.getElementById('name');
+alert('abc'); 
